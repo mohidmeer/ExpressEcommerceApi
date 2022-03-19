21e90d6af9f796621e7d9e6a1db0ccab9267c1da
Mohid Meer <mohidfauji@gmail.com>
2022-03-20 04:55:05 +0500

    local commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(authRoute);
+app.use("/users", userRoute);
+app.use("/products", productRoute);
+app.use("/carts", cartRoute);
+app.use("/orders", orderRoute);
+app.use("/checkout", stripeRoute);
+
+app.get('/',(req,res)=>{
+  res.status(200).json("INDEX PAGE REQUESTED")
+});
+app.get('**',(req,res)=>{
+  res.status(404).json("RESOURCE NOT FOUND")
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Backend server is running!");
+});
